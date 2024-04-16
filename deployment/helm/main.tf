e10f86327764ba967ee45a8d5f1bd3636dcb10bf
MSSkowron <mateusz.skowron@poczta.onet.pl>
2024-04-16 19:35:13 +0200

    Update

--- deployment/helm/main.tf
+++ deployment/helm/main.tf
@@ -36,7 +36,3 @@ data "kubernetes_service" "nats-lb" {
     namespace = var.namespace
   }
 }
-
-output "nats-lb-ip" {
-  value = data.kubernetes_service.nats-lb.status != null ? data.kubernetes_service.nats-lb.status.0.load_balancer.0.ingress.0.ip : null
-}

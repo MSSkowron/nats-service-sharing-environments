b037822c0907552a17077ee9e0f947081801c16a
MSSkowron <mateusz.skowron@poczta.onet.pl>
2024-04-18 12:52:32 +0200

    Add example setup

--- deployment/docker/setup.sh
+++ deployment/docker/setup.sh
@@ -3,3 +3,6 @@
 nats context save nats1 --server nats://admin:admin@localhost:4222 --description 'NATS Server localhost:4222'
 nats context save nats2 --server nats://admin:admin@localhost:4223 --description 'NATS Server localhost:4223'
 nats context save nats3 --server nats://admin:admin@localhost:4224 --description 'NATS Server localhost:4224'
+
+# nats context select nats1
+# nats stream add --cluster natscluster --subjects 'orders.*' --retention limits --max-msg-size 1MB --max-msgs 1000 --max-bytes 1GB --storage file --discard old --replicas 3 orders

add67acd8193bc3c6e1c15e820d7ab871be3e0d9
MSSkowron <mateusz.skowron@poczta.onet.pl>
2024-04-20 22:21:23 +0200

    Add script for light

---
+++ deployment/docker/devices/light/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "Użycie: $0 <ilość_kontenerów>"
+    exit 1
+fi
+
+docker build -t light .
+
+for ((i=1; i<=$1; i++)); do
+    rooms=("Kitchen" "Bathroom")
+    random_room=${rooms[RANDOM % ${#rooms[@]}]}
+
+    LIGHT_NAME="light-$i"
+    LIGHT_ROOM="$random_room"
+
+    docker run --net suu -e LIGHT_NAME="$LIGHT_NAME" -e LIGHT_ROOM="$LIGHT_ROOM" -d light &
+done
+
+wait

79b38a5dd9dd5899d919d4e4a7e9bb53b5875a3c
MSSkowron <mateusz.skowron@poczta.onet.pl>
2024-04-23 22:43:44 +0200

    Create one script

--- deployment/cluster/setup.sh
+++ deployment/cluster/setup.sh
@@ -5,4 +5,4 @@ nats context save nats2 --server nats://admin:admin@localhost:4223 --description
 nats context save nats3 --server nats://admin:admin@localhost:4224 --description 'NATS Server localhost:4224'
 
 nats context select nats1
-nats stream add --cluster natscluster --subjects "lights.>,airconditionersturn,airconditionersset,fridges.>,furnances.>,publishers.*" --retention limits --max-msg-size 1MB --max-msgs 1000 --max-bytes 1GB --storage file --discard old --replicas 3 devices
+nats stream add --cluster natscluster --subjects "lights.>,airconditionersturn,airconditionersset,fridges.>,furnances.>,publishers.*" --retention limits --max-msg-size 1MB --max-msgs 1000 --max-bytes 1GB --storage file --discard old --max-msgs-per-subject=-1 --max-age=-1 --dupe-window 2m0s --no-allow-rollup --no-deny-delete --no-deny-purge --replicas 3 devices
